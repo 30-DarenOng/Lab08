d5b9713fc3a72362e22aa080f1b2b93f01420f27
30-DarenOng <91870563+30-DarenOng@users.noreply.github.com>
2021-10-20 16:34:44 +0800

    Added gameover condition and update score

---
+++ Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        if (transform.position.y > 5)
+        {
+            Vector3 pos = transform.position;
+            pos.y = 5;
+            transform.position = pos;
+        }
+        if (transform.position.y < -3)
+        {
+            Vector3 pos = transform.position;
+            pos.y = -3;
+            transform.position = pos;
+        }
+
+
+
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
---
+++ Lab08/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnRedObject;
+    public GameObject SpawnBrownObject;
+    public GameObject SpawnGreenObject;
+
+    public int numRed = 1;
+    public int brown = 2;
+    public int numGreen = 3;
+    public int rand;
+
+    
+
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+    void RandomSpawn()
+    {
+        SpawnObjects();
+
+    }
+
+    void SpawnObjects()
+    {
+        rand = Random.Range(1, 4);
+        if (rand == 1)
+        {
+            PositionY = Random.Range(4, -4f);
+            this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+            Instantiate(SpawnRedObject, transform.position, transform.rotation);
+        }
+        if (rand == 2)
+        {
+            PositionY = Random.Range(4, -4f);
+            this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+            Instantiate(SpawnBrownObject, transform.position, transform.rotation);
+        }
+        if (rand == 3)
+        {
+            PositionY = Random.Range(4, -4f);
+            this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+            Instantiate(SpawnGreenObject, transform.position, transform.rotation);
+        }
+
+    }
+}
