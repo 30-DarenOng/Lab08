42d5df69afa7d7dbb0f033526e889205f82735d4
30-DarenOng <91870563+30-DarenOng@users.noreply.github.com>
2021-10-20 16:31:13 +0800

    Added gameover condition, added update score

--- Lab08/Assets/Scripts/Player.cs
+++ Lab08/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -36,5 +37,12 @@ public class Player : MonoBehaviour
 
 
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
     }
 }
---
+++ Lab08/Assets/Scripts/ScoreCollector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCollector : MonoBehaviour
+{
+    public int score;
+    public Text ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        ScoreText.text = ("Score: " + score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            score++;
+            ScoreText.text = ("Score: " + score);
+            Destroy(collision.gameObject);
+            
+        }
+    }
+}
