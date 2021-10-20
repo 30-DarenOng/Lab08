5582d919c84e6d9cac0b0a3fee78d5323753db6c
30-DarenOng <91870563+30-DarenOng@users.noreply.github.com>
2021-10-20 16:05:34 +0800

    Unity Project, added project. placed prefabs, Added Gameover scene, added random block spawn.

---
+++ Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        if (transform.position.y > 5)
+        {
+            Vector3 pos = transform.position;
+            pos.y = 5;
+            transform.position = pos;
+        }
+        if (transform.position.y < -3)
+        {
+            Vector3 pos = transform.position;
+            pos.y = -3;
+            transform.position = pos;
+        }
+
+
+
+
+    }
+}
